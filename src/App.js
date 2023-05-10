3f0f42af09cb0afb7b7d37f6e98ed15db4211789
Shivesh Dixit <dixitshivesh55@gmail.com>
2023-05-11 00:47:57 +0530

    adding hold dice functionality

--- src/App.js
+++ src/App.js
@@ -7,25 +7,64 @@ import { nanoid } from 'nanoid';
 function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
+
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+}
+
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
         newDice.push(
-          { value:Math.ceil(Math.random() * 6),
-            isHeld:false,
-            id:nanoid()
-          })
+            generateNewDie()
+          )
     }
-    console.log(newDice);
+    //console.log(newDice);
     return newDice
 }
-const diceElements = dice.map(item => <Dice value={item.value} key={item.id} />)
+
+function rollDice()
+{
+  setDice(olddice=>olddice.map(
+    (item)=>
+    {
+      if(item.isHeld==true) return item;
+      else
+      {
+        return generateNewDie(); 
+      }
+    }
+  ));
+}
+
+function holdDice(id)
+{
+  setDice(olddice=>olddice.map((item)=>
+  {
+   if(item.id==id)
+   {
+     return {...item, isHeld:!item.isHeld};
+   }
+   else 
+   return item;
+  }));
+}
+const diceElements = dice.map(item => <Dice value={item.value} key={item.id} id={item.id}
+  isHeld={item.isHeld}  holdDice={holdDice}
+  />)
+  
+
   return (
     <main>
       <div className='dice-container'>
     {diceElements}
     {/*dice.map(item => <Dice value={item} />)*/}
     </div>
+    <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   )
 }
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,8 +1,11 @@
 import React from "react"
 
 export default function Dice(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
     return (
-        <div className="dice-face">
+        <div className="dice-face" style={styles} onClick={()=>props.holdDice(props.id)}>
             <h2 className="dice-num">{props.value}</h2>
         </div>
     )
